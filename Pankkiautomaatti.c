a3aa27a4068fd599f883a3d05f83c6df9d7f4671
HavanneZ <havannez@gmail.com>
2015-10-25 23:24:26 +0200

    sisennys

--- Pankkiautomaatti.c
+++ Pankkiautomaatti.c
@@ -7,126 +7,126 @@ void nosto(void);
 void talletus (void);
 
 
-int main(void){
+int main(void) {
 
-  char tilinumero[256];
-  FILE * tili;
-  char pinkoodi[256];
-  char tarkistusPin[256];
-  int valinta;
+	char tilinumero[256];
+	FILE * tili;
+	char pinkoodi[256];
+	char tarkistusPin[256];
+	int valinta;
 
 
-  printf("\nAnna tilinumero: ");
-  fgets( tilinumero, 256, stdin );
+	printf("\nAnna tilinumero: ");
+	fgets( tilinumero, 256, stdin );
 
-  if( tilinumero[ strlen(tilinumero) -1] == '\n')
-     tilinumero[ strlen(tilinumero) -1] = '\0';
-  else 
-     lueRoskat();
+	if( tilinumero[ strlen(tilinumero) -1] == '\n')
+		tilinumero[ strlen(tilinumero) -1] = '\0';
+	else
+		lueRoskat();
 
-  strcat(tilinumero, ".tili");
+	strcat(tilinumero, ".tili");
 
-  if( ( tili = fopen( tilinumero, "r" ))!=NULL){
+	if( ( tili = fopen( tilinumero, "r" ))!=NULL) {
 
-     printf("Anna pinkoodisi: ");
- 
-     fgets( pinkoodi, 256, stdin );
+		printf("Anna pinkoodisi: ");
 
-     if( pinkoodi[ strlen(pinkoodi) -1] == '\n')
-        pinkoodi[ strlen(pinkoodi) -1] = '\0';
-     else
-        lueRoskat();
+		fgets( pinkoodi, 256, stdin );
 
-        
-     fgets( tarkistusPin, 256, tili );
-     
-     if( tarkistusPin[ strlen(tarkistusPin) -1] == '\n')
-        tarkistusPin[ strlen(tarkistusPin) -1] = '\0';
-   
-     	if( tarkistusPin[ strlen(tarkistusPin) -1] == '\r')
-        	tarkistusPin[ strlen(tarkistusPin) -1] = '\0';
-     
+		if( pinkoodi[ strlen(pinkoodi) -1] == '\n')
+			pinkoodi[ strlen(pinkoodi) -1] = '\0';
+		else
+			lueRoskat();
 
-     		if( strcmp( pinkoodi, tarkistusPin )  == 0 ){
-    			
-			printf("\nPin-koodi Ok!\n\n");    		
-        	printf("Tervetuloa! \n");
-	while (1) {      		
 
-        		paluu:
+		fgets( tarkistusPin, 256, tili );
+
+		if( tarkistusPin[ strlen(tarkistusPin) -1] == '\n')
+			tarkistusPin[ strlen(tarkistusPin) -1] = '\0';
+
+		if( tarkistusPin[ strlen(tarkistusPin) -1] == '\r')
+			tarkistusPin[ strlen(tarkistusPin) -1] = '\0';
+
+
+		if( strcmp( pinkoodi, tarkistusPin )  == 0 ) {
+
+			printf("\nPin-koodi Ok!\n\n");
+			printf("Tervetuloa! \n");
+			while (1) {
+
+				paluu:
 				printf("\n1. Saldo. \n");
 				printf("2. Nosto. \n");
 				printf("3. Talletus. \n");
 				printf("0. Lopeta. \n\n");
 				scanf("%d", &valinta);
 
-   				switch (valinta){
-					
-		case 1:
-			luesaldo();			
-			
-			break;
-						
-		case 2:
-			nosto();
-						
-						
-			break;
-						
-		case 3:
-			talletus();
-						
-						
-			break;
-						
-		case 0:
-			printf("Kiitos asioinnistasi!");
-						
-			break;
-		
-		default:
-			printf("Virheellinen valinta!\n");				
-			goto paluu;
-			
-			break;
-				}
-		if(valinta==0)
-		break;
-	}
+				switch (valinta) {
 
-   			
-   			
-     } else {
-	 
-        printf("Vaara pin-koodi, yrita uudelleen\n");
+					case 1:
+						luesaldo();
+
+						break;
+
+					case 2:
+						nosto();
+
+
+						break;
+
+					case 3:
+						talletus();
+
+
+						break;
+
+					case 0:
+						printf("Kiitos asioinnistasi!");
+
+						break;
+
+					default:
+						printf("Virheellinen valinta!\n");
+						goto paluu;
+
+						break;
+				}
+				if(valinta==0)
+					break;
+			}
+
+
+
+		} else {
+
+			printf("Vaara pin-koodi, yrita uudelleen\n");
 
 		}
-     
-  } else {
-     printf("\nVirheellinen tilinumero!\n");
-  }
-  
-	
 
-  return 0;
+	} else {
+		printf("\nVirheellinen tilinumero!\n");
+	}
 
-}
 
-void lueRoskat(void){
 
-   while(fgetc(stdin) != '\n');
+	return 0;
 
 }
 
-void luesaldo(void){
-	printf("Saldosi on: \n");
+void lueRoskat(void) {
+
+	while(fgetc(stdin) != '\n');
 		
 }
 
-void nosto(void){
+void luesaldo(void) {
+	printf("Saldosi on: \n");
+
+}
+
+void nosto(void) {
 	printf("Ilmoita nostettava summa: ");
 }
 
-void talletus(void){
+void talletus(void) {
 	printf("Ilmoita talletettava summa: ");
 }
